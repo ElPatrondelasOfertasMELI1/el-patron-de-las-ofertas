efc104207c6b04fb31bb51d115b5f4ce903977f8
ElPatrondelasOfertasMELI1 <conejotoystoys@gmail.com>
2026-06-24 22:52:38 -0600

    Create cupones.js

---
+++ cupones.js
@@ -0,0 +1,23 @@
+const cupones = [
+
+{
+categoria:"🔥 Relámpago",
+titulo:"12% OFF",
+descuento:"Tope $260",
+minimo:"Compra mínima $2,000",
+codigo:"MELI12YA",
+link:"https://meli.la/2n7c8Pu",
+activo:true
+},
+
+{
+categoria:"🏦 Bancario",
+titulo:"10% OFF",
+descuento:"Tope $700",
+minimo:"Compra mínima $4,500",
+codigo:"BBVAGOL",
+link:"https://meli.la/12Wui4z",
+activo:true
+}
+
+];
